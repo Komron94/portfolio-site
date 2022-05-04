34ba77cb29b8c7eb9c1f774a02341ee3a76483dc
Komron <example@gmail.com>
2022-05-04 15:59:39 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// let son = Number(prompt("Sonni kiriting:"));
+// let daraja = Number(prompt("Darajasi:"));
+// let power = function (x, y) {
+//     let javob = 1;
+//     for (let i = 0; i < y; i++) {
+//         javob *= x;
+//     }
+//     return javob;
+// };
+// console.log("JAvob:" + power(son, daraja));
